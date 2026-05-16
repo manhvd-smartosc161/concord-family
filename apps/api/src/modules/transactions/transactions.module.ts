b42b551508315989d1b4b9690e82c05001061e6b
manhvd-smartosc <manhvd@smartosc.com>
2026-05-17 01:26:52 +0700

    feat(api): wire family event notifications to transactions
    Import FamilyEventsModule in TransactionsModule so onJointTransaction
    notifications are emitted when transactions are logged. Spouse receives
    email alerts for shared fund activities.

--- apps/api/src/modules/transactions/transactions.module.ts
+++ apps/api/src/modules/transactions/transactions.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FamilyEventsModule } from '../../shared/notifications/family-events.module';
 import { Category } from '../categories/entities/category.entity';
 import { Fund } from '../funds/entities/fund.entity';
 import { Transaction } from './entities/transaction.entity';
@@ -7,7 +8,10 @@ import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, Fund, Category])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction, Fund, Category]),
+    FamilyEventsModule,
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
   exports: [TransactionsService],
